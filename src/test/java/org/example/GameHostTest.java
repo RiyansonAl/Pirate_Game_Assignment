8e0f258ca0c86dc40e28c7e1fd2330125095fe29
Riyanson Alfred <ralfr040@uottawa.ca>
2022-09-21 23:28:55 -0400

    U-TEST: 2 units tests for calculating the Score with a gold Card and then a diamond card

--- src/test/java/org/example/GameHostTest.java
+++ src/test/java/org/example/GameHostTest.java
@@ -130,6 +130,60 @@ class GameHostTest {
         assertEquals(score, host.calculateScore(player1, card, rolledDice));
     }
 
+    @Test
+    @DisplayName("Getting Scorce using a Gold fortune card")
+    void calculateScore(){
+        Player player1 = new Player();
+        Player player2 = new Player();
+        Player player3 = new Player();
+        Player[] players = {player1, player2, player3};
+
+        GameHost host = new GameHost(players);
+
+        GameHost.FortuneCard card = GameHost.FortuneCard.Gold;
+        GameHost.Dice[] rolledDice = {GameHost.Dice.Monkey, GameHost.Dice.Gold, GameHost.Dice.Gold,
+                GameHost.Dice.Gold, GameHost.Dice.Gold, GameHost.Dice.Skull,
+                GameHost.Dice.Gold, GameHost.Dice.Parrot};
+        int score = 1600;
+        assertEquals(score, host.calculateScore(player1, card, rolledDice));
+    }
+
+    @Test
+    @DisplayName("Getting Score using a Gold fortune card")
+    void calculateScore2(){
+        Player player1 = new Player();
+        Player player2 = new Player();
+        Player player3 = new Player();
+        Player[] players = {player1, player2, player3};
+
+        GameHost host = new GameHost(players);
+
+        GameHost.FortuneCard card = GameHost.FortuneCard.Gold;
+        GameHost.Dice[] rolledDice = {GameHost.Dice.Monkey, GameHost.Dice.Gold, GameHost.Dice.Gold,
+                GameHost.Dice.Gold, GameHost.Dice.Gold, GameHost.Dice.Skull,
+                GameHost.Dice.Gold, GameHost.Dice.Parrot};
+        int score = 1600;
+        assertEquals(score, host.calculateScore(player1, card, rolledDice));
+    }
+
+    @Test
+    @DisplayName("Getting Score using a Diamond fortune card")
+    void calculateScore3(){
+        Player player1 = new Player();
+        Player player2 = new Player();
+        Player player3 = new Player();
+        Player[] players = {player1, player2, player3};
+
+        GameHost host = new GameHost(players);
+
+        GameHost.FortuneCard card = GameHost.FortuneCard.Diamond;
+        GameHost.Dice[] rolledDice = {GameHost.Dice.Diamond, GameHost.Dice.Diamond, GameHost.Dice.Diamond,
+                GameHost.Dice.Diamond, GameHost.Dice.Diamond, GameHost.Dice.Diamond,
+                GameHost.Dice.Gold, GameHost.Dice.Diamond};
+        int score = 4900;
+        assertEquals(score, host.calculateScore(player1, card, rolledDice));
+    }
+
 
 
 
