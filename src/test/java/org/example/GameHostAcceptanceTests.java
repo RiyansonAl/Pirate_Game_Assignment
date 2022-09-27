ad4948c059ba8d41df9baa326abf60e08653193b
Riyanson Alfred <ralfr040@uottawa.ca>
2022-09-27 13:59:40 -0400

    A-TEST: Acceptance Test Line 47

--- src/test/java/org/example/GameHostAcceptanceTests.java
+++ src/test/java/org/example/GameHostAcceptanceTests.java
@@ -92,4 +92,39 @@ class GameHostAcceptanceTests {
         assertEquals(score, host.calculateScore(player1, card, secondRoll));
     }
 
+    @Test
+    @DisplayName("Acceptance Test Line 47")
+    void acceptanceTestLine47(){
+        System.out.println("A-TEST Line 47\n");
+        Player player1 = new Player(1);
+        Player player2 = new Player(2);
+        Player player3 = new Player(3);
+        Player[] players = {player1, player2, player3};
+
+        GameHost host = new GameHost(players);
+
+        GameHost.FortuneCard card = GameHost.FortuneCard.Gold;
+        GameHost.Dice[] rolledDice = {GameHost.Dice.Skull, GameHost.Dice.Parrot, GameHost.Dice.Parrot,
+                GameHost.Dice.Parrot, GameHost.Dice.Parrot, GameHost.Dice.Sword,
+                GameHost.Dice.Sword, GameHost.Dice.Sword};
+        int[] keepDice = {1,2,3,4};
+        GameHost.Dice[] riggedDice = {GameHost.Dice.Parrot, GameHost.Dice.Parrot, GameHost.Dice.Parrot,
+                GameHost.Dice.Parrot, GameHost.Dice.Skull, GameHost.Dice.Skull,
+                GameHost.Dice.Monkey, GameHost.Dice.Monkey};
+        int[] keepDice2 = {1,2,3,4,5,6};
+        GameHost.Dice[] riggedDice2 = {GameHost.Dice.Parrot, GameHost.Dice.Parrot, GameHost.Dice.Parrot,
+                GameHost.Dice.Parrot, GameHost.Dice.Skull, GameHost.Dice.Skull,
+                GameHost.Dice.Skull, GameHost.Dice.Monkey};
+
+        GameHost.Dice[] firstRoll = host.playerTurnStart(player1, card, rolledDice);
+        GameHost.Dice[] secondRoll = host.keepReRollDice(player1, keepDice, firstRoll, riggedDice, card);
+        GameHost.Dice[] thirdRoll = host.keepReRollDice(player1, keepDice, secondRoll, riggedDice2, card);
+
+        System.out.println(host.endTurn(player1));
+        System.out.println("\n\n\n");
+
+        int score = 0;
+        assertEquals(score, host.calculateScore(player1, card, thirdRoll));
+    }
+
 }
