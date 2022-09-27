37998c76f8fa818a5a03aef1d05df014ccacf5f3
Riyanson Alfred <ralfr040@uottawa.ca>
2022-09-27 15:17:42 -0400

    A-TEST: Acceptance Test Line 48

--- src/test/java/org/example/GameHostAcceptanceTests.java
+++ src/test/java/org/example/GameHostAcceptanceTests.java
@@ -127,4 +127,45 @@ class GameHostAcceptanceTests {
         assertEquals(score, host.calculateScore(player1, card, thirdRoll));
     }
 
+    @Test
+    @DisplayName("Acceptance Test Line 48")
+    void acceptanceTestLine48(){
+        System.out.println("A-TEST Line 48\n");
+        Player player1 = new Player(1);
+        Player player2 = new Player(2);
+        Player player3 = new Player(3);
+        Player[] players = {player1, player2, player3};
+
+        GameHost host = new GameHost(players);
+
+        GameHost.FortuneCard card = GameHost.FortuneCard.Gold;
+        GameHost.Dice[] rolledDice = {GameHost.Dice.Skull, GameHost.Dice.Parrot, GameHost.Dice.Parrot,
+                GameHost.Dice.Sword, GameHost.Dice.Sword, GameHost.Dice.Sword,
+                GameHost.Dice.Gold, GameHost.Dice.Gold};
+        int[] keepDice = {3,4,5,6,7};
+        GameHost.Dice[] riggedDice = {GameHost.Dice.Sword, GameHost.Dice.Sword, GameHost.Dice.Sword,
+                GameHost.Dice.Gold, GameHost.Dice.Gold, GameHost.Dice.Skull,
+                GameHost.Dice.Gold, GameHost.Dice.Gold};
+        int[] keepDice2 = {3,4,5,6,7};
+        GameHost.Dice[] riggedDice2 = {GameHost.Dice.Gold, GameHost.Dice.Gold, GameHost.Dice.Skull,
+                GameHost.Dice.Gold, GameHost.Dice.Gold, GameHost.Dice.Gold,
+                GameHost.Dice.Gold, GameHost.Dice.Gold};
+
+        System.out.println("First Roll");
+        GameHost.Dice[] firstRoll = host.playerTurnStart(player1, card, rolledDice);
+        System.out.println("Second Roll");
+        GameHost.Dice[] secondRoll = host.keepReRollDice(player1, keepDice, firstRoll, riggedDice, card);
+        System.out.println("Third Roll");
+        GameHost.Dice[] thirdRoll = host.keepReRollDice(player1, keepDice, secondRoll, riggedDice2, card);
+
+        player1.setUpdateScore(true);
+        int ObtainedScore = host.calculateScore(player1, card, thirdRoll);
+
+        System.out.println(host.endTurn(player1));
+        System.out.println("\n\n\n");
+
+        int score = 4800;
+        assertEquals(score, ObtainedScore);
+    }
+
 }
