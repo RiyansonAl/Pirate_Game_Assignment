a2f97239583692ee846692042b4a748bd8801997
Riyanson Alfred <ralfr040@uottawa.ca>
2022-09-28 11:58:37 -0400

    A-TEST: Acceptance Tests Line 106 - 107

--- src/test/java/org/example/GameHostAcceptanceTests.java
+++ src/test/java/org/example/GameHostAcceptanceTests.java
@@ -1313,6 +1313,66 @@ class GameHostAcceptanceTests {
         assertEquals(score, ObtainedScore);
     }
 
+    @Test
+    @DisplayName("Acceptance Test Line 106")
+    void acceptanceTestLine106(){
+        System.out.println("A-TEST: Acceptance Test Line 106\n");
+        Player player1 = new Player(1);
+        Player player2 = new Player(2);
+        Player player3 = new Player(3);
+        Player[] players = {player1, player2, player3};
+
+        GameHost host = new GameHost(players);
+
+        GameHost.FortuneCard card = GameHost.FortuneCard.Skulls;
+        GameHost.Dice[] rolledDice = {GameHost.Dice.Skull, GameHost.Dice.Monkey, GameHost.Dice.Parrot,
+                GameHost.Dice.Gold, GameHost.Dice.Gold, GameHost.Dice.Diamond,
+                GameHost.Dice.Diamond, GameHost.Dice.Diamond};
+
+        int numOfSkulls = host.getSkullCardType(player1, 2);
+        player1.setSkullCardNum(numOfSkulls);
+
+        GameHost.Dice[] firstRoll = host.playerTurnStart(player1, card, rolledDice);
+
+        int ObtainedScore = host.calculateScore(player1, card, firstRoll);
+
+        System.out.println(host.endTurn(player1));
+        System.out.println("\n\n\n");
+
+        int score = 0;
+        assertEquals(score, ObtainedScore);
+    }
+
+    @Test
+    @DisplayName("Acceptance Test Line 107")
+    void acceptanceTestLine107(){
+        System.out.println("A-TEST: Acceptance Test Line 107\n");
+        Player player1 = new Player(1);
+        Player player2 = new Player(2);
+        Player player3 = new Player(3);
+        Player[] players = {player1, player2, player3};
+
+        GameHost host = new GameHost(players);
+
+        GameHost.FortuneCard card = GameHost.FortuneCard.Skulls;
+        GameHost.Dice[] rolledDice = {GameHost.Dice.Skull, GameHost.Dice.Skull, GameHost.Dice.Parrot,
+                GameHost.Dice.Gold, GameHost.Dice.Gold, GameHost.Dice.Diamond,
+                GameHost.Dice.Diamond, GameHost.Dice.Diamond};
+
+        int numOfSkulls = host.getSkullCardType(player1, 1);
+        player1.setSkullCardNum(numOfSkulls);
+
+        GameHost.Dice[] firstRoll = host.playerTurnStart(player1, card, rolledDice);
+
+        int ObtainedScore = host.calculateScore(player1, card, firstRoll);
+
+        System.out.println(host.endTurn(player1));
+        System.out.println("\n\n\n");
+
+        int score = 0;
+        assertEquals(score, ObtainedScore);
+    }
+
 
 
 
